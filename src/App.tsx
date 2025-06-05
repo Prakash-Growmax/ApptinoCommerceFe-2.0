13ebb4858f36cc6aae9a4d2c425cdf229d8ed5c0
Ajitha2122 <ajitha@apptino.com>
2025-06-05 08:38:04 +0530

    recent changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import { AppRouter } from '@app/router';
 import { ErrorFallback, LoadingFallback } from '@components/organisms';
 import Test from './test';
 import { AppbarHeader } from './components/molecules/Appbar/Appbar';
-import { AppWithHeader } from './features/App/AppWithHeader';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AppWithHeader } from './app/router/AppWithHeader';
 
 function App() {
   const queryClient = new QueryClient();
