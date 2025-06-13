106916704d6fb562eeff383845fbb76bbe0f814a
Prakash Raj <prakashraj@apptino.com>
2025-06-13 14:40:59 +0530

    fix

--- src/features/support/routes/SupportDetails.tsx
+++ src/features/support/routes/SupportDetails.tsx
@@ -1,11 +1,44 @@
+import { useEffect } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
 import SupportCustomerCard from '../components/SupportCustomerCard/SupportCustomerCard';
+import { useGetSupportTicketDetails } from '../hook/useGetSupportTicketDetails';
+import { useGetSupportTicketFieldServices } from '../hook/useGetSupportTicketFieldServices';
 
 const SupportDetails = () => {
+  const {
+    data: fieldServicesData,
+    isLoading: isFieldServicesLoading,
+    error: fieldServicesError,
+    refetch: refetchFieldServices,
+  } = useGetSupportTicketFieldServices('dev3', 'ST0071');
+
+  const {
+    data: ticketDetailsData,
+    isLoading: isTicketDetailsLoading,
+    error: ticketDetailsError,
+    refetch: refetchTicketDetails,
+  } = useGetSupportTicketDetails('dev3', 'ST0071');
+
+  const isLoading = isFieldServicesLoading || isTicketDetailsLoading;
+  const error = fieldServicesError || ticketDetailsError;
+
+  const methods = useForm({
+    defaultValues: { fieldServicesData: null, supportTicketData: null },
+  });
+
+  useEffect(() => {
+    methods.reset({
+      fieldServicesData: fieldServicesData,
+      supportTicketData: ticketDetailsData,
+    });
+  }, [methods]);
+
   return (
-    <div>
+    <FormProvider {...methods}>
       Support Details
       <SupportCustomerCard />
-    </div>
+    </FormProvider>
   );
 };
 export default SupportDetails;
